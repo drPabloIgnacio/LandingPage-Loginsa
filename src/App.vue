c52243891ed913f8e7467a2940cfdfd5c12eb05d
Notorious Pablo Ignacio <90404984+drPabloIgnacio@users.noreply.github.com>
2022-02-28 10:46:20 -0300

    Primer commit
    :D

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-navigation :color="color" :flat="flat" />
+    <v-main>
+      <Header />
+
+    </v-main>
+    <v-scale-transition>
+      <v-btn
+      fab
+      v-show="fab"
+      v-scroll="onScroll"
+      darkfixed
+      bottom
+      color="secondary"
+      @click="toTop"
+      >
+        <v-icon>mdi-arrow-up</v-icon>
+      </v-btn>
+    </v-scale-transition>
+    <foote />
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+
+
+
+import foote from './components/Footer.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Header,
+    foote,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <v-navigation-drawer
+        v.model="drawer"
+        app
+        temporary
+        dark
+        >
+        <v-list>
+            <v-list-item>
+                <v-list-item-avatar>
+                    <!-- <img src="@/assets/img/logo.png" alt="logo"> -->
+                </v-list-item-avatar>
+                <v-list-item-conent>
+                    <v-list-item-tittle class="title">Loginsa</v-list-item-tittle>
+                    <v-list-item-subtitle>WEB</v-list-item-subtitle>
+                </v-list-item-conent>
+            </v-list-item>
+        </v-list>
+
+        <v-divider />
+
+        <v-list dense>
+            <v-list-item
+            v-for="([icon, text, link], i) in items"
+            :key="i"
+            link
+            @click="$vuetify.goTo(link)"
+            >
+                <v-list-item-icon class="justify-center">
+                    <v-icon>{{ icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                    <v-list-item-tittle class="subtitile-1">
+                    {{ text }}
+                    </v-list-item-tittle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar
+        app
+        :color="color"
+        :flat="flat"
+        dark
+        class="px-15"
+        :class="{ expand: flat}"
+        >
+            <v-toolbr-title>
+                <!-- <v-img src="@/assets/img/logo.png" max-width="50px" /> -->
+            </v-toolbr-title>
+            <v-spacer />
+            <v-app-bar-nav-icon
+                @click.stop="drawer = !drawer"
+                class="mr-4"
+                v-if="isXs"
+            />
+            <div v-else>
+                <v-btn text @click="$vuetify.goTo('#hero')">
+                    <span class="mr-2">Home</span>
+                </v-btn>
+            </div>
+
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
